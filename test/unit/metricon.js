98c47dedeb7e20d5125adf43a7f28974624ced34
Sergio Gimeno <gimeno.sergio@gmail.com>
2017-04-20 01:45:54 +0200

    tests for report

--- test/unit/metricon.js
+++ test/unit/metricon.js
@@ -1,7 +1,50 @@
 const test = require('tape')
 const metricon = require('../../lib/metricon')
+// const reportSpec = require('../support/report-spec.json')
 
-test('Metricon exports', assert => {
+test('Metricon exports a report function', assert => {
   assert.equal(typeof metricon.report, 'function', 'Should have a report method')
   assert.end()
+})
+
+test('Metricon report expected output', assert => {
+  const options = {
+    organization: 'sgimeno',
+    repositories: ['metricon'],
+    milestone: 'Test milestone'
+  }
+  assert.ok(metricon.report(options) instanceof Promise, 'Should return a Promise')
+  assert.end()
+})
+
+test('Metricon report expected output', assert => {
+  const options = {
+    organization: 'sgimeno',
+    repositories: ['metricon'],
+    milestone: 'Test milestone',
+    waffleBoard: 'sgimeno/metricon'
+  }
+
+  metricon.report(options)
+    .then(report => {
+      // console.log('REPORT', report)
+      assert.ok(report, 'Should return a data')
+      // assert.deepEqual(report, reportSpec, 'Should match the spec')
+      assert.end()
+    })
+    .catch(error => {
+      console.log('ERR', error)
+      assert.fail(error, 'Should not get here')
+      assert.end()
+    })
+})
+
+test('Metricon report without options fails', assert => {
+  try {
+    metricon.report()
+  } catch (e) {
+    assert.ok(e, 'Should throw an exception')
+  } finally {
+    assert.end()
+  }
 })
