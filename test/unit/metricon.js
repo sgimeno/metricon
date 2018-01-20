1a15a1a3e7e08d71039fbad72238f5190dbf7d29
Sergio Gimeno <gimeno.sergio@gmail.com>
2018-01-20 13:54:00 +0100

    feat: pass credentials to metricon client using .auth() method

--- test/unit/metricon.js
+++ test/unit/metricon.js
@@ -2,8 +2,15 @@ const test = require('tape')
 const metricon = require('../../lib/metricon')
 // const reportSpec = require('../support/report-spec.json')
 
-test('Metricon exports a report function', assert => {
+test('Metricon exports report, auth function', assert => {
   assert.equal(typeof metricon.report, 'function', 'Should have a report method')
+  assert.equal(typeof metricon.auth, 'function', 'Should have a auth method')
+  assert.end()
+})
+
+test('Metricon exports auth creates a connector', assert => {
+  metricon.auth({ token: process.env.GH_TOKEN_TEST })
+  assert.ok(metricon.connectors.github, 'Should have a github connector')
   assert.end()
 })
 
@@ -13,6 +20,7 @@ test('Metricon report expected output', assert => {
     repositories: ['metricon'],
     milestone: 'Test milestone'
   }
+  metricon.auth({ token: process.env.GH_TOKEN_TEST })
   assert.ok(metricon.report(options) instanceof Promise, 'Should return a Promise')
   assert.end()
 })
@@ -24,6 +32,7 @@ test('Metricon report expected output', assert => {
     milestone: 'Test milestone'
   }
 
+  metricon.auth({ token: process.env.GH_TOKEN_TEST })
   metricon.report(options)
     .then(report => {
       // console.log('REPORT', report)
